b7ce43e21c9b4728f80327eaaa6a4e4e0b1ff034
navidasaman <navidasaman@gmail.com>
2023-09-11 01:14:18 +0200

    registration- and login form with tailwindcss
    frontend + backend

---
+++ client/src/App.tsx
@@ -0,0 +1,108 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+interface User {
+  username: string;
+  password: string;
+}
+
+const App: React.FC = () => {
+  const [loginUser, setLoginUser] = useState<User>({
+    username: '',
+    password: '',
+  });
+
+  const [registerUser, setRegisterUser] = useState<User>({
+    username: '',
+    password: '',
+  });
+
+  const handleLoginInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setLoginUser({ ...loginUser, [event.target.name]: event.target.value });
+  };
+
+  const handleRegisterInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setRegisterUser({ ...registerUser, [event.target.name]: event.target.value });
+  };
+
+  const handleLogin = () => {
+    axios.post(`${process.env.REACT_APP_API_URL}${process.env.REACT_APP_LOGIN_ENDPOINT}`, loginUser)
+      .then(response => {
+        console.log(response.data);
+        // Handle successful login
+      })
+      .catch(error => {
+        console.error(error);
+        // Handle login error
+      });
+  };
+
+  const handleRegister = () => {
+    axios.post(`${process.env.REACT_APP_API_URL}${process.env.REACT_APP_REGISTER_ENDPOINT}`, registerUser)
+      .then(response => {
+        console.log(response.data);
+        // Handle successful registration
+      })
+      .catch(error => {
+        console.error(error);
+        // Handle registration error
+      });
+  };
+
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="bg-slate-200 p-8 shadow-md rounded-md">
+        <h2 className="text-2xl font-bold mb-4">Login</h2>
+        <input
+          type="text"
+          name="username"
+          value={loginUser.username}
+          onChange={handleLoginInputChange}
+          placeholder="Username"
+          className="border border-gray-300 rounded-md px-3 py-2 mb-4 w-full"
+        />
+        <input
+          type="password"
+          name="password"
+          value={loginUser.password}
+          onChange={handleLoginInputChange}
+          placeholder="Password"
+          className="border border-gray-300 rounded-md px-3 py-2 mb-4 w-full"
+        />
+        <button
+          onClick={handleLogin}
+          className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600"
+        >
+          Login
+        </button>
+      </div>
+      <div className="bg-green-100 p-8 shadow-md rounded-md ml-4">
+        <h2 className="text-2xl font-bold mb-4">Register</h2>
+        <input
+          type="text"
+          name="username"
+          value={registerUser.username}
+          onChange={handleRegisterInputChange}
+          placeholder="Username"
+          className="border border-gray-300 rounded-md px-3 py-2 mb-4 w-full"
+        />
+        <input
+          type="password"
+          name="password"
+          value={registerUser.password}
+          onChange={handleRegisterInputChange}
+          placeholder="Password"
+          className="border border-gray-300 rounded-md px-3 py-2 mb-4 w-full"
+        />
+        <button
+          onClick={handleRegister}
+          className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600"
+        >
+          Register
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ server/server.ts
@@ -0,0 +1,109 @@
+import express, { Request, Response } from 'express';
+import mysql from 'mysql';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+import cookieParser from 'cookie-parser';
+import cors from 'cors'
+import dotenv from 'dotenv';
+dotenv.config();
+
+const app = express(); // instance of express app
+const PORT = 3001; // port number which the server listens for incoming requests
+app.use(express.json()); // middleware to the Express application to parse JSON data in request bodies
+app.use(cors()); //  middleware to the Express application to enable CORS to be able for backend to access API from frontend
+app.use(cookieParser());
+
+// Creates a connection to the MySQL database
+const connection = mysql.createConnection({
+  host: '[sensitive data, edited out]',
+  user: '[sensitive data, edited out]',
+  password: '[sensitive data, edited out]',
+  port: 3306,
+  database: '[sensitive data, edited out]',
+});
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting to MySQL database:', err);
+    return;
+  }
+  console.log('Connected to MySQL database');
+});
+
+
+// Register endpoint
+app.post(`${process.env.REGISTER_ENDPOINT}`, (req: Request, res: Response) => {
+  const { username, password } = req.body;
+
+  // Hash the password
+  bcrypt.hash(password, 10, (err, hash) => {
+    if (err) {
+      console.error('Error hashing password:', err);
+      res.status(500).json({ error: 'Internal server error' });
+      return;
+    }
+
+    // Insert user into the database
+    connection.query(
+      'INSERT INTO [sensitive data, edited out] (username, password) VALUES (?, ?)',
+      [username, hash],
+      (err, result) => {
+        if (err) {
+          console.error('Error registering user:', err);
+          res.status(500).json({ error: 'Internal server error' });
+          return;
+        }
+        res.status(201).json({ message: 'User registered successfully' });
+      }
+    );
+  });
+});
+
+// Login endpoint
+app.post(`${process.env.LOGIN_ENDPOINT}`, (req: Request, res: Response) => {
+  const { username, password } = req.body;
+
+  // Check if the user exists
+  connection.query(
+    'SELECT * FROM [sensitive data, edited out] WHERE username = ?',
+    [username],
+    (err, result) => {
+      if (err) {
+        console.error('Error logging in:', err);
+        res.status(500).json({ error: 'Internal server error' });
+        return;
+      }
+
+      if (result.length === 0) {
+        res.status(401).json({ error: 'Invalid username or password' });
+        return;
+      }
+
+      const user = result[0];
+
+      // Compare the password
+      bcrypt.compare(password, user.password, (err, isMatch) => {
+        if (err) {
+          console.error('Error comparing passwords:', err);
+          res.status(500).json({ error: 'Internal server error' });
+          return;
+        }
+
+        if (!isMatch) {
+          res.status(401).json({ error: 'Invalid username or password' });
+          return;
+        }
+
+        // Generate JWT token
+        const token = jwt.sign({ userId: user.id }, 'your_secret_key');
+
+        // Set the token in a cookie
+        res.cookie('token', token, { httpOnly: true });
+        res.status(200).json({ message: 'Logged in successfully' });
+      });
+    }
+  );
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
